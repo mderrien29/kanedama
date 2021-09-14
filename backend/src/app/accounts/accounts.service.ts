516c42b8f92f5baeb573ba80444cad9c105928c9
Martial <martial.derrien-ext@kering.com>
2021-09-14 13:32:19 +0200

    fix: apply transaction in reverse order

--- backend/src/app/accounts/accounts.service.ts
+++ backend/src/app/accounts/accounts.service.ts
@@ -4,21 +4,30 @@ import * as chunkDateRange from 'chunk-date-range';
 import { ApiService } from './api.service';
 import { TransactionDto } from 'src/common/dtos/transaction.dto';
 import { DateInterval } from 'src/common/interfaces';
+import { AccountDto } from 'src/common/dtos/account.dto';
 
 @Injectable()
 export class AccountsService {
   constructor(private readonly apiService: ApiService) {}
 
-  public async getAllUserHistory(): Promise<TransactionDto[]> {
-    const accounts = await this.apiService.getAccounts();
+  public async getUserAccounts(): Promise<AccountDto[]> {
+    return await this.apiService.getAccounts();
+  }
 
+  public async getUserFullHistory(
+    accounts: AccountDto[],
+  ): Promise<TransactionDto[]> {
     const allUserData = accounts.map(
       async (account) =>
         await this.getAllTransactionsFromAccount(account.account_id),
     );
 
-    // WE CAN NOT DO A FLAT HERE, WE HAVE TO ORDER BY DATE
-    return (await Promise.all(allUserData)).flat(1);
+    return (await Promise.all(allUserData))
+      .flat(1)
+      .sort(
+        (a, b) =>
+          new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime(),
+      );
   }
 
   private async getAllTransactionsFromAccount(
@@ -44,16 +53,25 @@ export class AccountsService {
   }
 
   // Note: this is valid for amounts in a single currency
-  public calculateAccountAmountOverTime(
+  public calculateAccountAmountOverTimeFromCurrent(
     transactions: TransactionDto[],
-    initialAccountValue = 0,
+    currentAccountValue = 6357,
   ): number[] {
-    const amountOverTime = [initialAccountValue];
+    const amountOverTime = [currentAccountValue];
 
     for (let i = 0; i < transactions.length; i++) {
-      amountOverTime[i + 1] = amountOverTime[i] + transactions[i].amount;
+      amountOverTime[i + 1] =
+        amountOverTime[i] - transactions[transactions.length - i - 1].amount;
     }
 
+    // could optimise
+    console.dir(Math.floor(Math.max(...amountOverTime)));
+    console.dir(Math.floor(Math.min(...amountOverTime)));
+
     return amountOverTime;
+  }
+
+  public sumAccountsCurrentAmount(accounts: AccountDto[]): number {
+    return accounts.reduce((sum, account) => (sum += account.current), 0);
   }
 }
--- backend/src/app/app.service.ts
+++ backend/src/app/app.service.ts
@@ -10,21 +10,18 @@ export class AppService {
   }
 
   async getAnswer(): Promise<string> {
-    // return JSON.stringify(await this.accountsService.getAccounts());
-    // return JSON.stringify(
-    //   await this.apiService.getTransactions(
-    //     'afcf2bd0-9a72-11e9-86ef-07c2f863fee7',
-    //     new Date('2019-08-01'),
-    //     new Date('2020-07-31'),
-    //   ),
-    // );
-    const transactions =
-      await this.accountsService.getAllUserHistory();
-
-    return JSON.stringify(
-      this.accountsService.calculateAccountAmountOverTime(
-        transactionsPerAccount,
-      ),
+    const accounts = await this.accountsService.getUserAccounts();
+    const currentAccountValue =
+      this.accountsService.sumAccountsCurrentAmount(accounts);
+    const transactions = await this.accountsService.getUserFullHistory(
+      accounts,
     );
+    const amountOverTime =
+      this.accountsService.calculateAccountAmountOverTimeFromCurrent(
+        transactions,
+        currentAccountValue,
+      );
+
+    return JSON.stringify(amountOverTime);
   }
 }
